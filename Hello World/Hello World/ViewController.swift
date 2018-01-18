0affa834cc5d7d216eb84c05fa807c59ff12cbda
Camille Frazier <cafz89@mwc-120255.dhcp.missouri.edu>
2018-01-18 13:36:05 -0600

    Completed Hello World

--- Hello World/Hello World/ViewController.swift
+++ Hello World/Hello World/ViewController.swift
@@ -10,9 +10,13 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var helloLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        helloLabel.text = "Welcome!"
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +24,11 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func sayHello(_ sender: UIButton) {
+    }
+    
+    
+    @IBAction func clearLabel(_ sender: UIButton) {
+    }
 }
 
