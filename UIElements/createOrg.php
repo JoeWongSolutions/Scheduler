a47078922c8d635cd046d2d3086cfea98fd1a4e7
William Smith <williamsmithtannis013@gmail.com>
2018-04-12 16:00:31 -0500

    V1.0 of createOrg complete

--- UIElements/createOrg.php
+++ UIElements/createOrg.php
@@ -41,6 +41,7 @@ if(!($strNumber = empty($_POST['strNumber']) ? false : testInput($_POST['strNumb
 //Encryped pass
 $pass = hash("sha512",$pass);
 
+
 //Database connection
 require_once "db.conf";
 $mysqli = new mysqli($servername, $username, $password, $dbname);
@@ -48,32 +49,98 @@ if ($mysqli->connect_error) {
     echo('Error: ' . $mysqli->connect_errno . ' ' . $mysqli->connect_error);
     exit;
 }
+//see if manager exist
+$sql = "SELECT managerID FROM managers WHERE managerID=(?)";
+if (!($stmt = $mysqli->prepare($sql))) {
+    echo "Manager select Prepare failed: (" . $mysqli->errno . ") " . $mysqli->error;
+    exit;
+}
+if (!$stmt->bind_param("s", $userName)) {
+    echo "Manager select Binding parameters failed: (" . $stmt->errno . ") " . $stmt->error;
+    exit;
+}
+if (!$stmt->execute()) {
+    echo "Manager select Execute failed: (" . $stmt->errno . ") " . $stmt->error;
+    exit;
+}
+if ($res = $stmt->get_result()) {
+
+    if(($res->num_rows) != 0){
+    echo "User already exists";
+    exit;
+    }
+}
+//See if org already exists
+$sql = "SELECT orgName FROM organizations WHERE orgName=(?) AND storeNumber=(?)";
+if (!($stmt = $mysqli->prepare($sql))) {
+    echo "Org to see if exists select Prepare failed: (" . $mysqli->errno . ") " . $mysqli->error;
+    exit;
+}
+if (!$stmt->bind_param("si", $orgName, $strNumber)) {
+    echo "Org to see if exists Binding parameters failed: (" . $stmt->errno . ") " . $stmt->error;
+    exit;
+}
+if (!$stmt->execute()) {
+    echo "Org to see if exists Execute failed: (" . $stmt->errno . ") " . $stmt->error;
+    exit;
+}
+if ($res = $stmt->get_result()) {
+
+    if(($res->num_rows) != 0){
+        echo "Org already exists";
+        exit;
+    }
+}
+
+
 $sql = "INSERT INTO organizations (orgName, locationCity, locationState, storeNumber) VALUES
 ((?),(?),(?),(?))";
 if (!($stmt = $mysqli->prepare($sql))) {
-    echo "Prepare failed: (" . $mysqli->errno . ") " . $mysqli->error;
+    echo "Org insert Prepare failed: (" . $mysqli->errno . ") " . $mysqli->error;
     exit;
 }
 if (!$stmt->bind_param("sssi", $orgName, $orgCity, $orgState, $strNumber)) {
-    echo "Binding parameters failed: (" . $stmt->errno . ") " . $stmt->error;
+    echo "Org insert Binding parameters failed: (" . $stmt->errno . ") " . $stmt->error;
     exit;
 }
 if (!$stmt->execute()) {
-    echo "Execute failed: (" . $stmt->errno . ") " . $stmt->error;
+    echo "Org insert Execute failed: (" . $stmt->errno . ") " . $stmt->error;
     exit;
 }
-$sql = "INSERT INTO managers (orgName, locationCity, locationState, storeNumber) VALUES
+
+$sql = "SELECT orgID FROM organizations WHERE orgName=(?) AND locationCity=(?) AND locationState=(?) AND storeNumber=(?)";
+if (!($stmt = $mysqli->prepare($sql))) {
+    echo "Org select Prepare failed: (" . $mysqli->errno . ") " . $mysqli->error;
+    exit;
+}
+if (!$stmt->bind_param("sssi", $orgName, $orgCity, $orgState, $strNumber)) {
+    echo "Org select Binding parameters failed: (" . $stmt->errno . ") " . $stmt->error;
+    exit;
+}
+if (!$stmt->execute()) {
+    echo "Org select Execute failed: (" . $stmt->errno . ") " . $stmt->error;
+    exit;
+}
+if (!($res = $stmt->get_result())) {
+    echo "Org select Failed to get result";
+    exit;
+}else{
+    $orgID = ($res->fetch_assoc())["orgID"];
+}
+
+
+$sql = "INSERT INTO managers (managerID, orgID, CreationDate, pass) VALUES
 ((?),(?),(?),(?))";
 if (!($stmt = $mysqli->prepare($sql))) {
-    echo "Prepare failed: (" . $mysqli->errno . ") " . $mysqli->error;
+    echo "Manager insert Prepare failed: (" . $mysqli->errno . ") " . $mysqli->error;
     exit;
 }
-if (!$stmt->bind_param("sssi", $orgName, $orgCity, $orgState, $strNumber)) {
-    echo "Binding parameters failed: (" . $stmt->errno . ") " . $stmt->error;
+if (!$stmt->bind_param("sssi", $userName, $orgID, $creationDate, $pass)) {
+    echo " Manager insert Binding parameters failed: (" . $stmt->errno . ") " . $stmt->error;
     exit;
 }
 if (!$stmt->execute()) {
-    echo "Execute failed: (" . $stmt->errno . ") " . $stmt->error;
+    echo "Manager insert Execute failed: (" . $stmt->errno . ") " . $stmt->error;
     exit;
 }
 $mysqli->close();
