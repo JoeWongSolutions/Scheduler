e9f4273b83b64a6ddf7fd2635d4a42266d6aabf4
William Smith <williamsmithtannis013@gmail.com>
2018-04-17 20:09:26 -0500

    V0.91 editOrg.php

--- UIElements/editOrg.php
+++ UIElements/editOrg.php
@@ -38,21 +38,27 @@ if (!$stmt->bind_param("i", $orgID)) {
 if (!$stmt->execute()) {
     echo "Original User Data Execute failed: (" . $stmt->errno . ") " . $stmt->error;
     exit;
-}else{
-    $res = $stmt->get_result()->fetch_assoc();
+}
+if ($res = $stmt->get_result()) {
+    if(($res->num_rows) == 0){
+        echo "Org does not exist";
+        exit;
+    }else{
+        $row = $res->fetch_assoc();
+    }
 }
 
 if(!($name = empty($_POST['name']) ? false : testInput($_POST['name']))){
-    $name = $res["name"];
+    $name = $row["name"];
 }
 if(!($locationCity = empty($_POST['locationCity']) ? false : testInput($_POST['locationCity']))){
-    $locationCity = $res["locationCity"];
+    $locationCity = $row["locationCity"];
 }
 if(!($locationState = empty($_POST['locationState']) ? false : testInput($_POST['locationState']))){
-    $locationState = $res["locationState"];
+    $locationState = $row["locationState"];
 }
 if(!($storeNumber = empty($_POST['storeNumber']) ? false : testInput($_POST['storeNumber']))){
-    $storeNumber = $res["storeNumber"];
+    $storeNumber = $row["storeNumber"];
 }
 
 $sql = "UPDATE organizations SET orgName = (?), locationCity = (?), locationState = (?), storeNumber = (?) WHERE orgID = (?)";
